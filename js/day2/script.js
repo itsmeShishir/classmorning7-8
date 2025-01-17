a0d8bc61cde6a3b784c3d0509ae77bafe8127af1
tilak mahata <mahatatluck@gmail.com>
2025-01-17 08:31:23 +0545

    add all file

---
+++ js/day2/script.js
@@ -0,0 +1,23 @@
+// let age =17;
+// if(age >= 18) {
+// console.log("he or she is 18");
+// }else{
+//     console.log("he or she is not 18 real age is "+ age )
+
+// } 
+let day = prompt("Please enter the day of the week:");
+if (day =="Sunday") {
+  alert("Today is Sunday ");
+  } else if (day == "Monday") {
+  alert("Today is Monday");
+} else if (day == "Thursday") {
+  alert("today is thursday");
+} else if (day == "Friday") {
+  alert("Today is Friday");
+} else if (day == "Saturday") {
+  alert("Today is Saturday");
+} else {
+  alert("None of the above days. Try again.");
+}
+
+
